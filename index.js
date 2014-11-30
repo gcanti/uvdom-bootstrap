20e5acf8961d8b2ee7535d69e577d784f83d6f18
gcanti <giulio.canti@gmail.com>
2014-11-30 18:05:22 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  Textbox: require('./lib/Textbox'),
+  Checkbox: require('./lib/Checkbox'),
+  Select: require('./lib/Select'),
+  Radio: require('./lib/Radio'),
+  Fieldset: require('./lib/Fieldset')
+};
