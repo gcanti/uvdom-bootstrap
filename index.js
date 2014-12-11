cf68633decc121dce846b35ddc5f8702b5d240b0
gcanti <giulio.canti@gmail.com>
2014-12-11 18:15:13 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 module.exports = {
-  Textbox: require('./lib/Textbox'),
-  Checkbox: require('./lib/Checkbox'),
-  Select: require('./lib/Select'),
-  Radio: require('./lib/Radio'),
-  Fieldset: require('./lib/Fieldset')
+  getAddon: require('./lib/getAddon'),
+  getBreakpoints: require('./lib/getBreakpoints'),
+  getButton: require('./lib/getButton'),
+  getButtonGroup: require('./lib/getButtonGroup'),
+  getCheckbox: require('./lib/getCheckbox'),
+  getCol: require('./lib/getCol'),
+  getFieldset: require('./lib/getFieldset'),
+  getFormGroup: require('./lib/getFormGroup'),
+  getHelpBlock: require('./lib/getHelpBlock'),
+  getInputGroup: require('./lib/getInputGroup'),
+  getLabel: require('./lib/getLabel'),
+  getOffsets: require('./lib/getOffsets'),
+  getOptGroup: require('./lib/getOptGroup'),
+  getOption: require('./lib/getOption'),
+  getRadio: require('./lib/getRadio'),
+  getRow: require('./lib/getRow'),
+  getSelect: require('./lib/getSelect'),
+  getTextbox: require('./lib/getTextbox')
 };
