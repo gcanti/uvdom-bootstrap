ab4748042b47b9367aa2998689bf345dd0c4d301
gcanti <giulio.canti@gmail.com>
2014-12-12 11:30:42 +0100

    added form exports

---
+++ form.js
@@ -0,0 +1,20 @@
+module.exports = {
+  getAddon: require('./lib/getAddon'),
+  getBreakpoints: require('./lib/getBreakpoints'),
+  getButton: require('./lib/getButton'),
+  getButtonGroup: require('./lib/getButtonGroup'),
+  getCheckbox: require('./lib/getCheckbox'),
+  getCol: require('./lib/getCol'),
+  getFieldset: require('./lib/getFieldset'),
+  getFormGroup: require('./lib/getFormGroup'),
+  getHelpBlock: require('./lib/getHelpBlock'),
+  getInputGroup: require('./lib/getInputGroup'),
+  getLabel: require('./lib/getLabel'),
+  getOffsets: require('./lib/getOffsets'),
+  getOptGroup: require('./lib/getOptGroup'),
+  getOption: require('./lib/getOption'),
+  getRadio: require('./lib/getRadio'),
+  getRow: require('./lib/getRow'),
+  getSelect: require('./lib/getSelect'),
+  getTextbox: require('./lib/getTextbox')
+};
